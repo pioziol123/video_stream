c8ef99a1fc2da85d0cda3d63ff116d29ebfb9205
Piotr Ziółkowski <pioziol@gmail.com>
2019-12-09 12:44:12 +0100

    optimized with zmq

--- client.py
+++ client.py
@@ -4,20 +4,26 @@ import websockets
 from io import BytesIO
 import numpy as np
 from base64 import b64encode
+import time
+import zmq
+from zmq.asyncio import Context, ZMQEventLoop
 
+ctx =Context()
 vc = cv2.VideoCapture(0)
 if not vc.isOpened():
     exit()
 
 
 async def stream():
-    async with websockets.connect("ws://localhost:8765") as websocket:
-        rval = True
-        result = True
-        while rval and result:
-            rval, frame = vc.read()
-            result, image = cv2.imencode(".png", frame)
-            await websocket.send(b64encode(image.tobytes()))
+    socket = ctx.socket(zmq.PUB)
+    socket.bind("tcp://127.0.0.1:5555")
+    rval = True
+    result = True
+    while rval and result:
+        rval, frame = vc.read()
+        result, image = cv2.imencode(".jpg", frame)
+        await socket.send_multipart([b"1", b64encode(image.tobytes())])
+        await asyncio.sleep(1000/15000)
 
-
+asyncio.set_event_loop(ZMQEventLoop())
 asyncio.get_event_loop().run_until_complete(stream())
--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import asyncio
 import websockets
+import zmq
+from zmq.asyncio import Context, Poller, ZMQEventLoop
 
+Url = 'tcp://127.0.0.1:5555'
+Ctx = Context()
 receivers = set()
 
 
@@ -8,19 +12,31 @@ async def sendPicture(picture):
     try:
         for receiver in receivers:
             await receiver.send(picture)
-    except Exception:
+    except Exception as e:
+        print(str(e))
         receivers.remove(receiver)
 
 
 async def handleMessage(websocket, path):
     async for message in websocket:
-        if message == "receiver":
+        if websocket not in receivers:
             receivers.add(websocket)
-        elif websocket not in receivers:
-            await sendPicture(message)
 
+async def handleZMQMessage():
+    subscriber = Ctx.socket(zmq.SUB)
+    subscriber.connect(Url)
+    subscriber.setsockopt(zmq.SUBSCRIBE, b"1")
+    poller = Poller()
+    poller.register(subscriber, zmq.POLLOUT)
+    while True:
+        topic, data = await subscriber.recv_multipart()
+        await sendPicture(data)
 
+asyncio.set_event_loop(ZMQEventLoop())
 asyncio.get_event_loop().run_until_complete(
-    websockets.serve(handleMessage, "localhost", 8765)
+    asyncio.gather(
+        websockets.serve(handleMessage, "localhost", 8765),
+        handleZMQMessage()
+    )
 )
-asyncio.get_event_loop().run_forever()
+# asyncio.get_event_loop().run_forever()
